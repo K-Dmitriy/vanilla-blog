c34fd8f34454dc1626118dc2800a238510cd8af9
Дмитрий Козлов <d.kozlov@hott.local>
2020-03-26 14:27:04 +0300

    add validation

--- src/core/form.js
+++ src/core/form.js
@@ -9,9 +9,15 @@ export class Form {
 
     Object.keys(this.controls).forEach(control => {
       value[control] = this.form[control].value;
-    })
+    });
 
     return value
+  }
+
+  clear() {
+    Object.keys(this.controls).forEach(control => {
+      this.form[control].value = '';
+    });
   }
 
   isValid() {
@@ -24,10 +30,25 @@ export class Form {
       validators.forEach(validator => {
         isValid = validator(this.form[control].value) && isValid;
       });
+      
+      isValid ? clearError(this.form[control]) : setError(this.form[control]);
 
       isFormValid = isFormValid && isValid;
     });
 
     return isFormValid
   }
+}
+
+function setError($control) {
+  clearError($control);
+  const error = '<p class="validation-error">Введите корректное значение</p>';
+  $control.classList.add('invalid');
+  $control.insertAdjacentHTML('afterend', error)
+}
+
+function clearError($control) {
+  $control.classList.remove('invalid');
+
+  if ($control.nextSibling) $control.closest('.form-control').removeChild($control.nextSibling);
 }
